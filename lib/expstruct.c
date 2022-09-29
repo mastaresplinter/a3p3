16fa6ee27fe7c9337fd861a856d1195acdd4fbe3
mastaresplinter <mackandn@gmail.com>
2022-09-29 11:13:22 +0200

    Added methods from eariler assignments
    Added mathods from earlier assignemnts and tried to assign each method to its own thread

--- lib/expstruct.c
+++ lib/expstruct.c
@@ -6,24 +6,26 @@
 /*
  * Modified by Wagner Morais on Aug 2022.
  */
- 
+
 #include <stddef.h>
 #include <stdlib.h>
 #include "expstruct.h"
 #include "rpi-systimer.h"
 
-double faculty(int n) 
+double faculty(int n)
 {
-  if (n != 0) {
+  if (n != 0)
+  {
     return n * faculty((int)(n - 1));
-  } else
+  }
+  else
     return 1;
 }
 
-double ppow(int base, int exp) 
+double ppow(int base, int exp)
 {
   double result = 1.0;
-  while (exp != 0) 
+  while (exp != 0)
   {
     result = result * base;
     exp--;
@@ -31,25 +33,26 @@ double ppow(int base, int exp)
   return result;
 }
 
-ExpStruct *iexp(int x){
-    ExpStruct *e = malloc(sizeof(ExpStruct));
-    double term = 0;
-    double tempFraction = 0;
-    e->expInt = 1+x;
-    e->expFraction = 0;
-    int i = 2;
-    while(1)
-    { 
-      term = (double)ppow(x, i) / faculty(i);
-      e->expInt += (int)term;
-      tempFraction += (term - (int) term);
+ExpStruct *iexp(int x)
+{
+  ExpStruct *e = malloc(sizeof(ExpStruct));
+  double term = 0;
+  double tempFraction = 0;
+  e->expInt = 1 + x;
+  e->expFraction = 0;
+  int i = 2;
+  while (1)
+  {
+    term = (double)ppow(x, i) / faculty(i);
+    e->expInt += (int)term;
+    tempFraction += (term - (int)term);
 
-      if (term < 0.0001)
-        break;
-      else
-        i++;
-    }
-    e->expInt += (int)tempFraction;
-    e->expFraction = (tempFraction-(int)tempFraction)*100;
-    return e;
+    if (term < 0.0001)
+      break;
+    else
+      i++;
+  }
+  e->expInt += (int)tempFraction;
+  e->expFraction = (tempFraction - (int)tempFraction) * 100;
+  return e;
 }
